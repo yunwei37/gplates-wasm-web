73c31b0f85e7bbdad14618d179cdac04fa606f2e
yunwei37 <1067852565@qq.com>
2023-05-01 01:13:55 +0800

    init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "gplatesnextweb": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3gplatesnextwebstoragefeatures": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
