572ee26440356f0d6aeef49617f837b21122b7ba
yunwei37 <1067852565@qq.com>
2023-05-01 14:55:47 +0800

    add d3

--- src/components/GplatesD3Visualization/GplatesD3Visualization.js
+++ src/components/GplatesD3Visualization/GplatesD3Visualization.js
@@ -1,7 +1,53 @@
-import React from 'react';
+// src/components/GplatesD3Visualization/GplatesD3Visualization.js
+import React, { useRef, useEffect } from 'react';
+import * as d3 from 'd3';
+import * as topojson from 'topojson';
+import './GplatesD3Visualization.css';
 
-function GplatesD3Visualization() {
-  return <div>Gplates D3 Visualization</div>;
-}
+const GplatesD3Visualization = () => {
+  const svgRef = useRef();
+
+  useEffect(() => {
+    // 将原始的 JavaScript 代码转换为适用于 React 的代码
+    // 例如，将原始的 d3 代码放在这里
+  }, []);
+
+  return (
+    <div className="gplates-d3-visualization">
+      <div style={{ width: '960px', textAlign: 'center', margin: '0 auto' }}>
+        <h1 id="time-label" style={{ fontSize: '3em', margin: '0' }}>
+          140 Ma
+        </h1>
+      </div>
+      <div style={{ width: '962px', overflow: 'hidden', margin: '0 auto' }}>
+        <svg ref={svgRef} style={{ width: '960px', height: '500px' }}></svg>
+        <div style={{ textAlign: 'center' }}>
+        <label>Time:</label>
+            <input id="recon-time" type="number" min="0" step="1" max="550" value="140" style="margin-right:10px;"/>
+            <label>Projection:</label> 
+            <select id="select-projection" style="margin-right:10px;">
+              <option value="orthographic" >Orthographic</option>
+              <option value="equirectangular" selected>Rectangular</option>
+            </select>
+            <label style="display:none;">Function:</label>
+            <select id="select-function" style="display:none;">
+              <option value="1" selected>Reconstruct Points</option>
+              <option value="2" >Reconstruct Feature Collection</option>
+            </select>
+            <br></br>
+            <textarea id="args-textarea" rows="2" cols="100">116,39,151,-33, -74, 40, 37, 55, -43,-22, 18, 14</textarea>
+            <br></br>
+            <input type="button" id="commit" value="Refresh Map"/>
+            <input type="button" id="show-url" value="Show Request URL"/>
+            <input type="button" id="show-data" value="Show Returned Raw Data"/>
+        </div>
+        <br />
+        <div id="request-url"></div>
+        <br />
+        <div id="raw-data"></div>
+      </div>
+    </div>
+  );
+};
 
 export default GplatesD3Visualization;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,46 +1,12 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { API } from 'aws-amplify';
-import { withAuthenticator, Button, Text, Flex, Heading } from '@aws-amplify/ui-react';
-import { createNote, deleteNote } from '../graphql/mutations';
-import { listNotes } from '../graphql/queries';
+import React, { useState } from 'react';
+import { withAuthenticator, Button, Flex, Heading } from '@aws-amplify/ui-react';
 import Tabs from '../components/Tabs/Tabs';
 import FileManager from '../components/FileManager/FileManager';
 import GplatesD3Visualization from '../components/GplatesD3Visualization/GplatesD3Visualization';
 import GplatesCodeEditor from '../components/GplatesCodeEditor/GplatesCodeEditor';
 
 function MainPage({ signOut }) {
-  const [notes, setNotes] = useState([]);
-  const [activeTab, setActiveTab] = useState('Tab1');
-
-  const fetchNotes = useCallback(async () => {
-    const result = await API.graphql({
-      query: listNotes,
-      authMode: 'AMAZON_COGNITO_USER_POOLS'
-    })
-    setNotes(result.data.listNotes.items)
-  }, [setNotes])
-
-  const handleCreateNote = useCallback(async () => {
-    await API.graphql({
-      query: createNote,
-      variables: { input: { text: window.prompt("New note") } },
-      authMode: 'AMAZON_COGNITO_USER_POOLS'
-    })
-    fetchNotes()
-  }, [fetchNotes])
-
-  const handleDeleteNote = useCallback(async (id) => {
-    await API.graphql({
-      query: deleteNote,
-      variables: { input: { id: id } },
-      authMode: 'AMAZON_COGNITO_USER_POOLS'
-    })
-    fetchNotes()
-  }, [fetchNotes])
-
-  useEffect(() => {
-    fetchNotes()
-  }, [fetchNotes])
+  const [activeTab, setActiveTab] = useState('File Manager');
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
@@ -48,19 +14,14 @@ function MainPage({ signOut }) {
 
   return (
     <Flex direction={"column"}>
-      <Tabs tabs={['Tab1', 'Tab2', 'Tab3']} onTabChange={handleTabChange} />
+      <Flex justifyContent={'space-between'}>
+        <Heading level={1}>My App</Heading>
+        <Button onClick={signOut}>Sign Out</Button>
+      </Flex>
+      <Tabs tabs={['File Manager', 'Gplates D3 Visualization', 'Gplates Code Editor']} onTabChange={handleTabChange} />
       {activeTab === 'File Manager' && <FileManager />}
       {activeTab === 'Gplates D3 Visualization' && <GplatesD3Visualization />}
       {activeTab === 'Gplates Code Editor' && <GplatesCodeEditor />}
-      <Flex justifyContent={'space-between'}>
-        <Heading level={1}>My notes</Heading>
-        <Button onClick={signOut}>Sign Out</Button>
-      </Flex>
-      {notes.map(note => <Flex alignItems={'center'}>
-        <Text>{note.text}</Text>
-        <Button onClick={() => handleDeleteNote(note.id)}>Remove</Button>
-      </Flex>)}
-      <Button onClick={handleCreateNote}>Add Note</Button>
     </Flex>
   );
 }
